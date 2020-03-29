7b22ea7b20327946100ef7d2eaaefbd7cd1a91c2
engin ustun <enginustun@outlook.com>
2020-03-29 23:25:41 +0300

    feat: Update templates table and migrations api

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const checkQueryTemplate = require('./lib/middleware/query-face-template');
 const queryFace = require('./lib/middleware/query-face');
 const setDatabase = require('./lib/databases').setDatabase;
-const runMigrations = require('./lib/run-migrations');
+const {
+  runMigrations,
+  listMigrations,
+  makeMigrations,
+  latestMigrations,
+  rollbackMigrations,
+  upMigrations,
+  downMigrations,
+  currentVersionMigrations,
+} = require('./lib/migrations');
 const runSeeds = require('./lib/run-seeds');
 const { addEvent: addBeforeEvent } = require('./lib/query-events/before');
 const { addEvent: addAfterEvent } = require('./lib/query-events/after');
@@ -75,6 +84,109 @@ module.exports.setDatabase = setDatabase;
  * queryFaceNode.runMigrations(databases);
  */
 module.exports.runMigrations = runMigrations;
+
+/**
+ * Creates a new migration, with the name of the migration being added.
+ *
+ * @memberof queryFaceNode.
+ * @function makeMigrations
+ * @param {Object} db knex database to execute migrations on it
+ * @param {String} name migration name
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.makeMigrations(db, 'dbName', config);
+ */
+module.exports.makeMigrations = makeMigrations;
+
+/**
+ * Runs all migrations that have not yet been run.
+ *
+ * @memberof queryFaceNode.
+ * @function latestMigrations
+ * @param {Object} db knex database to execute latest function on it
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.latestMigrations(db, config);
+ */
+module.exports.latestMigrations = latestMigrations;
+
+/**
+ * Rolls back the latest migration group.
+ * If the all parameter is truthy, all applied migrations will be rolled back instead of just the last batch.
+ * The default value for this parameter is false.
+ *
+ * @memberof queryFaceNode.
+ * @function rollbackMigrations
+ * @param {Object} db knex database to execute rollback function on it
+ * @param {boolean} [all=false] if truthy all applied migrations will be rolled back
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.rollbackMigrations(db, [true|false], config);
+ */
+module.exports.rollbackMigrations = rollbackMigrations;
+
+/**
+ * Runs the specified (by config.name parameter) or the next chronological migration that has not yet be run.
+ *
+ * @memberof queryFaceNode.
+ * @function upMigrations
+ * @param {Object} db knex database to execute up function on it
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.upMigrations(db, config);
+ */
+module.exports.upMigrations = upMigrations;
+
+/**
+ * Will undo the specified (by config.name parameter) or the last migration that was run.
+ *
+ * @memberof queryFaceNode.
+ * @function downMigrations
+ * @param {Object} db knex database to execute down function on it
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.downMigrations(db, config);
+ */
+module.exports.downMigrations = downMigrations;
+
+/**
+ * Retrieves and returns the current migration version, as a promise.
+ * If there aren't any migrations run yet, returns "none" as the value for the currentVersion.
+ *
+ * @memberof queryFaceNode.
+ * @function currentVersionMigrations
+ * @param {Object} db knex database to execute currentVersion function on it
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.currentVersionMigrations(db, config);
+ */
+module.exports.currentVersionMigrations = currentVersionMigrations;
+
+/**
+ * Will return list of completed and pending migrations
+ *
+ * @memberof queryFaceNode.
+ * @function listMigrations
+ * @param {Object} db knex database to execute list function on it
+ * @param {Object} [config={}] migration config
+ * @example
+ * const queryFaceNode = require('query-face-node');
+ * const db = require('./databases').dbName;
+ * queryFaceNode.listMigrations(db, config);
+ */
+module.exports.listMigrations = listMigrations;
 
 /**
  * @memberof queryFaceNode.
